4c11f18c5c4c37847b7e8dde24d20b29ea1ecce2
kasun <kasunsmbox@hotmail.com>
2019-09-09 12:27:59 +0530

    added auto installations script

--- ins.py
+++ ins.py
@@ -1,15 +1,16 @@
 import os
 
 try:
-    os.system("pip install numpy")
-    os.system("pip install wheel")
-    os.system("pip install matplotlib")
-    os.system("pip install seaborn")
-    os.system("pip install sklearn")
-    os.system("pip install keras")
-    os.system("pip install opencv-python")
-    os.system("pip install tensorflow")
-    os.system("pip install --upgrade setuptools")
+    os.system("sudo pip install --upgrade setuptools")
+    os.system("sudo pip install numpy")
+    os.system("sudo pip install wheel")
+    os.system("sudo pip install matplotlib")
+    os.system("sudo pip install seaborn")
+    os.system("sudo pip install sklearn")
+    os.system("sudo pip install keras")
+    os.system("sudo pip install opencv-python")
+    os.system("sudo pip install tensorflow")
+    
 
 except Exception as e:
     print(e)
