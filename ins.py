0c4b0a975edf70e08c017ece2d35e8ec5a96655b
root <root@localhost.localdomain>
2020-01-24 17:34:39 +0530

    added some libraries

--- ins.py
+++ ins.py
@@ -10,7 +10,12 @@ try:
     os.system("pip install keras")
     os.system("pip install opencv-python")
     os.system("pip install tensorflow")
-    
+    os.system("pip install tf-nightly")
+    os.system("pip install cloud-tpu-client")
+    os.system("pip install pillow")
+
+    # export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.1-cp27-none-linux_x86_64.whl
+    # pip install — upgrade $TF_BINARY_URL
 
 except Exception as e:
     print(e)
