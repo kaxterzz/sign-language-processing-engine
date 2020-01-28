372063e0a8291b7f6ca5102e073969752178bc0c
root <root@localhost.localdomain>
2020-01-28 13:08:53 +0530

    added ini file

--- app/ins.py
+++ app/ins.py
@@ -15,9 +15,5 @@ try:
     os.system("pip install pillow")
     os.system("pip install socketio")
 
-    # export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.1-cp27-none-linux_x86_64.whl
-    # pip install — upgrade $TF_BINARY_URL
-    # https://medium.com/analytics-vidhya/sign-language-recognition-using-cnn-and-opencv-beginner-level-72091ca35a19
-
 except Exception as e:
     print(e)
