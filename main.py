b4af4e17a0869eb02d84446a22fc5bbb303d74d9
Thilanka Kasun <kasunsmbox@hotmail.com>
2020-01-28 23:39:08 +0530

    added prediction function

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import socketio
 import string
 import random
 from base64 import b64decode
-from keras.models import load_model
-from keras.preprocessing import image
+from predict import predict
 
 app = Flask(__name__)
 
@@ -29,33 +28,6 @@ def randomString(stringLength=10):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-def predict(img):
-    model = load_model("testmodel3.h5")
-
-    test_image = image.load_img(img,color_mode="grayscale",target_size=(28,28,1))
-    # test_image = x_train[3]
-    # plt.imshow(x_train[3].reshape(28,28))
-    #print(test_image.format)
-    #print(test_image.mode)
-    #print(test_image.size)
-
-    test_image = image.img_to_array(test_image)
-    test_image  = test_image.reshape((-1,) + test_image.shape)
-
-    print(test_image.dtype)
-    print(test_image.shape)
-
-    y_pred = model.predict_classes(test_image)
-    print(y_pred)
-    prediction = y_pred[0]
-    classname = y_pred[0]
-    print("Class: ",classname)
-    print(y_pred)
-    f = open('res.txt', 'w')
-        f.write("Class: ",classname)
-        f.close()
-    return prediction
-
 @app.route('/upload-image', methods=['POST'])
 def upload_files():
     try:
@@ -74,9 +46,9 @@ def upload_files():
             f.close()
             # status = static_file.save(path + img_name)
             # full_file = path + static_file_name
-            predict(str(random_file_name))
-            return "true"
-            #return jsonify(res)
+            res = predict(str(random_file_name))
+            #return "true"
+            return jsonify(res)
         else:
             return "false"
 
